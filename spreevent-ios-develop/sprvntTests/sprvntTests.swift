bf5ea754ca8cf1f44293a1a403adce938bac6f83
sergeikichev <60177867+sergeikichev@users.noreply.github.com>
2022-04-23 17:58:36 +0200

    Add files via upload

---
+++ spreevent-ios-develop/sprvntTests/sprvntTests.swift
@@ -0,0 +1,36 @@
+//
+//  sprvntTests.swift
+//  sprvntTests
+//
+//  Created by Sergei Kichev  on 27.08.2020.
+//  Copyright © 2020 Sergei Kichev . All rights reserved.
+//
+
+import XCTest
+@testable import sprvnt
+
+class sprvntTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
